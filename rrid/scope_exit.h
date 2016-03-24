77534c6e280de61267fbfbfbbd78ddd0ea5109bd
pp-qq <p_qp__q@163.com>
2016-03-24 21:57:45 +0800

    stash

---
+++ rrid/scope_exit.h
@@ -0,0 +1,12 @@
+#ifndef ORG_PP_QQ_COMMON_RRID_SCOPE_EXIT_H
+#define ORG_PP_QQ_COMMON_RRID_SCOPE_EXIT_H
+
+#include <functional>
+
+namespace pp_qq {
+
+
+
+}
+
+#endif // ORG_PP_QQ_COMMON_RRID_SCOPE_EXIT_H
