a97a14c2d772ca1c2827fceeb675ce446293129b
pp__qq <p_qp__q@163.com>
2016-04-25 15:21:38 +0800

    feat(string): 完善了 ExtendedStdString 对象的操作,更像 std::string

--- src/string/extended_std_string.h
+++ src/string/extended_std_string.h
@@ -11,6 +11,24 @@ struct ExtendedStdString : public std::string {
     inline ExtendedStdString(ExtendedStdString &&);
     inline ExtendedStdString(const ExtendedStdString &);
 
+    /* 这里要指定 explicit,不然 std::string 类型就可以自动转化为 ExtendedStdString 类型.
+     * 但是 ExtendedStdString 类型已经可以自动转化为 std::string 类型了,所以可能会造成很
+     * 多二义性.
+     *
+     * 相比 std::string 类型自动转化为 ExtendedStdString 类型;ExtendedStdString 更适合
+     * 转化为 std::string 类型.如下:
+     *
+     *    std::string str;
+     *    // 此时会 ExtendedStdString tmp(str); estr = tmp;会拷贝一份 str.
+     *    const ExtendedStdString &estr = str;
+     *
+     *    ExtendedStdString estr;
+     *    // 此时并不会有啥子拷贝.
+     *    const std::string &str = estr;
+     */
+    explicit inline ExtendedStdString(const std::string &);
+    explicit inline ExtendedStdString(std::string &&);
+
     using std::string::operator=;
 
     inline const value_type& cfront() const;
@@ -32,6 +50,15 @@ ExtendedStdString::ExtendedStdString()
 {
 }
 
+ExtendedStdString::ExtendedStdString(const std::string &other):
+    std::string(other)
+{
+}
+
+ExtendedStdString::ExtendedStdString(std::string &&other):
+    std::string(std::move(other))
+{
+}
 
 ExtendedStdString::ExtendedStdString(ExtendedStdString &&other):
     std::string{static_cast<std::string&&>(other)}
@@ -103,5 +130,222 @@ auto ExtendedStdString::const_raw_data() const -> const value_type*
 }
 
 
+
+
+inline ExtendedStdString
+operator+(const ExtendedStdString& lhs,const ExtendedStdString& rhs)
+{
+    return ExtendedStdString(static_cast<const std::string&>(lhs) + static_cast<const std::string&>(rhs));
+}
+
+
+inline ExtendedStdString
+operator+(ExtendedStdString&& lhs,const ExtendedStdString& rhs)
+{
+    return ExtendedStdString(static_cast<std::string&&>(lhs) + static_cast<const std::string&>(rhs));
+}
+
+
+inline ExtendedStdString
+operator+(const ExtendedStdString& lhs,ExtendedStdString&& rhs)
+{
+    return ExtendedStdString(static_cast<const std::string&>(lhs) + static_cast<std::string&&>(rhs));
+}
+
+
+inline ExtendedStdString
+operator+(ExtendedStdString&& lhs,ExtendedStdString&& rhs)
+{
+    return ExtendedStdString(static_cast<std::string&&>(lhs) + static_cast<std::string&&>(rhs));
+}
+
+
+inline ExtendedStdString
+operator+(const char* lhs,const ExtendedStdString& rhs)
+{
+    return ExtendedStdString(lhs + static_cast<const std::string&>(rhs));
+}
+
+
+inline ExtendedStdString
+operator+(const char* lhs,ExtendedStdString&& rhs)
+{
+    return ExtendedStdString(lhs + static_cast<std::string&&>(rhs));
+}
+
+
+inline ExtendedStdString
+operator+(char lhs,const ExtendedStdString& rhs)
+{
+    return ExtendedStdString(lhs + static_cast<const std::string&>(rhs));
+}
+
+
+inline ExtendedStdString
+operator+(char lhs,ExtendedStdString&& rhs)
+{
+    return ExtendedStdString(lhs + static_cast<std::string&&>(rhs));
+}
+
+
+inline ExtendedStdString
+operator+(const ExtendedStdString& lhs,const char* rhs)
+{
+    return ExtendedStdString(static_cast<const std::string&>(lhs) + rhs);
+}
+
+
+inline ExtendedStdString
+operator+(ExtendedStdString&& lhs,const char* rhs)
+{
+    return ExtendedStdString(static_cast<std::string&&>(lhs) + rhs);
+}
+
+
+inline ExtendedStdString
+operator+(const ExtendedStdString& lhs,char rhs)
+{
+    return ExtendedStdString(static_cast<const std::string&>(lhs) + rhs);
+}
+
+
+inline ExtendedStdString
+operator+(ExtendedStdString&& lhs,char rhs)
+{
+    return ExtendedStdString(static_cast<std::string&&>(lhs) + rhs);
+}
+
+inline bool
+operator==(const ExtendedStdString& lhs,const ExtendedStdString& rhs)
+{
+    return static_cast<const std::string&>(lhs) == static_cast<const std::string&>(rhs);
+}
+
+
+
+inline bool
+operator==(const ExtendedStdString& lhs,const ExtendedStdString::value_type* rhs)
+{
+    return static_cast<const std::string&>(lhs) == rhs;
+}
+
+inline bool
+operator==(const ExtendedStdString::value_type* lhs,const ExtendedStdString& rhs)
+{
+    return lhs == static_cast<const std::string&>(rhs);
+}
+
+
+inline bool
+operator!=(const ExtendedStdString& lhs,const ExtendedStdString& rhs)
+{
+    return static_cast<const std::string&>(lhs) != static_cast<const std::string&>(rhs);
+}
+
+
+inline bool
+operator!=(const ExtendedStdString::value_type* lhs,const ExtendedStdString& rhs)
+{
+    return lhs != static_cast<const std::string&>(rhs);
+}
+
+
+inline bool
+operator!=(const ExtendedStdString& lhs,const ExtendedStdString::value_type* rhs)
+{
+    return static_cast<const std::string&>(lhs) != rhs;
+}
+
+
+inline bool
+operator<(const ExtendedStdString& lhs,const ExtendedStdString& rhs)
+{
+    return static_cast<const std::string&>(lhs) < static_cast<const std::string&>(rhs);
+}
+
+
+inline bool
+operator<(const ExtendedStdString& lhs,const ExtendedStdString::value_type* rhs)
+{
+    return static_cast<const std::string&>(lhs) < rhs;
+}
+
+
+inline bool
+operator<(const ExtendedStdString::value_type* lhs,const ExtendedStdString& rhs)
+{
+    return lhs < static_cast<const std::string&>(rhs);
+}
+
+
+inline bool
+operator>(const ExtendedStdString& lhs,const ExtendedStdString& rhs)
+{
+    return static_cast<const std::string&>(lhs) > static_cast<const std::string&>(rhs);
+}
+
+
+inline bool
+operator>(const ExtendedStdString& lhs,const ExtendedStdString::value_type* rhs)
+{
+    return static_cast<const std::string&>(lhs) > rhs;
+}
+
+
+inline bool
+operator>(const ExtendedStdString::value_type* lhs,const ExtendedStdString& rhs)
+{
+    return lhs > static_cast<const std::string&>(rhs);
+}
+
+
+inline bool
+operator<=(const ExtendedStdString& lhs,const ExtendedStdString& rhs)
+{
+    return static_cast<const std::string&>(lhs) <= static_cast<const std::string&>(rhs);
+}
+
+
+inline bool
+operator<=(const ExtendedStdString& lhs,const ExtendedStdString::value_type* rhs)
+{
+    return static_cast<const std::string&>(lhs) <= rhs;
+}
+
+
+inline bool
+operator<=(const ExtendedStdString::value_type* lhs,const ExtendedStdString& rhs)
+{
+    return lhs <= static_cast<const std::string&>(rhs);
+}
+
+
+inline bool
+operator>=(const ExtendedStdString& lhs,const ExtendedStdString& rhs)
+{
+    return static_cast<const std::string&>(lhs) >= static_cast<const std::string&>(rhs);
+}
+
+
+inline bool
+operator>=(const ExtendedStdString& lhs,const ExtendedStdString::value_type* rhs)
+{
+    return static_cast<const std::string&>(lhs) >= rhs;
+}
+
+
+inline bool
+operator>=(const ExtendedStdString::value_type* lhs,const ExtendedStdString& rhs)
+{
+    return lhs >= static_cast<const std::string&>(rhs);
+}
+
+
+inline void
+swap(ExtendedStdString& lhs, ExtendedStdString& rhs)
+{
+    lhs.swap(rhs);
+    return ;
+}
 
 #endif // ORG_PP_QQ_COMMON_STRING_EXTENDED_STD_STRING_H
