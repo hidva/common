d9da928a2459736124fe14c3570c1081c6a9de06
pp-qq <p_qp__q@163.com>
2017-01-08 17:19:50 +0800

    test(kmpdfa): 增加测试用例

--- src/string/kmpdfa_test.cc
+++ src/string/kmpdfa_test.cc
@@ -22,6 +22,21 @@ TEST(KMPDFATest, CaseSensitive) {
     EXPECT_EQ(2U, kmpdfa.Run("2b"));
     EXPECT_EQ(9U, kmpdfa.Run("ABCAAACBA"));
     EXPECT_EQ(3U, kmpdfa.Run("abcaaacba"));
+
+    kmpdfa.Compile("a", true);
+    EXPECT_EQ(3U, kmpdfa.Run("bbb"));
+    EXPECT_EQ(0U, kmpdfa.Run("A"));
+    EXPECT_EQ(3U, kmpdfa.Run("bcdAefg"));
+
+    kmpdfa.Compile("abcd", true);
+    EXPECT_EQ(2U, kmpdfa.Run("2b"));
+    EXPECT_EQ(3U, kmpdfa.Run("ABCABCDEFG"));
+    EXPECT_EQ(6U, kmpdfa.Run("bcdabcabCDefg"));
+
+    kmpdfa.Compile("aaa", true);
+    EXPECT_EQ(2U, kmpdfa.Run("2b"));
+    EXPECT_EQ(3U, kmpdfa.Run("ABCAAACBA"));
+    EXPECT_EQ(3U, kmpdfa.Run("abcaAacba"));
 }
 
 TEST(KMPDFATest, CaseInsensitive) {
@@ -44,4 +59,19 @@ TEST(KMPDFATest, CaseInsensitive) {
     EXPECT_EQ(2U, kmpdfa.Run("2b"));
     EXPECT_EQ(3U, kmpdfa.Run("ABCAAACBA"));
     EXPECT_EQ(3U, kmpdfa.Run("abcaAacba"));
+
+    kmpdfa.Compile("a", false);
+    EXPECT_EQ(3U, kmpdfa.Run("bbb"));
+    EXPECT_EQ(1U, kmpdfa.Run("A"));
+    EXPECT_EQ(3U, kmpdfa.Run("bcdaefg"));
+
+    kmpdfa.Compile("abcd", false);
+    EXPECT_EQ(2U, kmpdfa.Run("2b"));
+    EXPECT_EQ(10U, kmpdfa.Run("ABCABCDEFG"));
+    EXPECT_EQ(6U, kmpdfa.Run("bcdabcabcdefg"));
+
+    kmpdfa.Compile("aaa", false);
+    EXPECT_EQ(2U, kmpdfa.Run("2b"));
+    EXPECT_EQ(9U, kmpdfa.Run("ABCAAACBA"));
+    EXPECT_EQ(3U, kmpdfa.Run("abcaaacba"));
 }
