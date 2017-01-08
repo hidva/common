654baff69615617778445ccfdb31a63a2c31d05f
pp-qq <p_qp__q@163.com>
2017-01-08 17:29:03 +0800

    fix(acdfa): 测试逻辑错误

--- src/string/acdfa_test.cc
+++ src/string/acdfa_test.cc
@@ -24,17 +24,17 @@ TEST(ACDFATest, test1) {
     EXPECT_ANY_THROW(acdfa.Compile({"", "", ""}));
 
     acdfa.Compile({"He", "SHe", "Hem", "He"});
-    EXPECT_EQ(Sort({{0, 3}, {1, 3}, {1, 3}, {1, 4}}), Sort(acdfa.Run("SHem")));
+    EXPECT_EQ(Sort({{0, 3}, {1, 2}, {1, 2}, {1, 3}}), Sort(acdfa.Run("SHem")));
     EXPECT_EQ(Sort({}), Sort(acdfa.Run("hehe")));
-    EXPECT_EQ(Sort({{2, 5}, {3, 5}, {3, 5}, {3, 6}}), Sort(acdfa.Run("heSHemhe")));
+    EXPECT_EQ(Sort({{2, 3}, {3, 2}, {3, 2}, {3, 3}}), Sort(acdfa.Run("heSHemhe")));
 
     acdfa.Compile({"He", "hE"});
     EXPECT_EQ(Sort({}), Sort(acdfa.Run("he")));
-    EXPECT_EQ(Sort({{0, 2}, {4, 6}}), Sort(acdfa.Run("HehehE")));
-    EXPECT_EQ(Sort({{2, 4}, {4, 6}}), Sort(acdfa.Run("heHehEhe")));
+    EXPECT_EQ(Sort({{0, 2}, {4, 2}}), Sort(acdfa.Run("HehehE")));
+    EXPECT_EQ(Sort({{2, 2}, {4, 2}}), Sort(acdfa.Run("heHehEhe")));
 
     acdfa.Compile({"HE", "she", "hEm", "hE"}, true);
-    EXPECT_EQ(Sort({{0, 3}, {1, 3}, {1, 3}, {1, 4}}), Sort(acdfa.Run("SHEM")));
-    EXPECT_EQ(Sort({{0, 2}, {2, 4}}), Sort(acdfa.Run("hehe")));
+    EXPECT_EQ(Sort({{0, 3}, {1, 2}, {1, 2}, {1, 3}}), Sort(acdfa.Run("SHEM")));
+    EXPECT_EQ(Sort({{0, 2}, {2, 2}}), Sort(acdfa.Run("hehe")));
     EXPECT_EQ(Sort({}), Sort(acdfa.Run("abcd")));
 }
